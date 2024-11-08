cc95268102a101eda26e2900aee40984c4ab2cb5
danielmontesalejandres <danielmontesalejandres@gmail.com>
2024-11-08 18:18:03 +0100

    librerias instaladas y conexion efectuada

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import os
 import json
-from web3 import Web3
-from dotenv import load_dotenv
+from web3 import Web3 # type: ignore
+from dotenv import load_dotenv # type: ignore
 
 load_dotenv()
 
@@ -24,3 +24,35 @@ with open("artifacts/Saludo_metadata.json") as f:
         print("Connected to Arbitrum RPC endpoint")
     else:
         print("Connection Failed")
+
+    contract_address = CONTRACT
+    contract_abi = ABI
+
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+    account_address = WALLET
+    private_key = PRIV_KEY
+
+    result = contract.functions.leerSaludo().call()
+
+    print("-" * 50)
+    print(f"Ultimo saludo: {result}")
+
+
+    function_data = contract.functions.guardarSaludo("Hola desde python recargado II").build_transaction({
+        'from': account_address,
+        'gas': 5000000,
+        'gasPrice': w3.to_wei('10', 'gwei'),
+        'nonce': w3.eth.get_transaction_count(WALLET),
+        'chainId': 421614,
+    })
+
+    signed_transaction = w3.eth.account.sign_transaction(function_data, private_key)
+    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction)
+
+    print(f"HASH: {transaction_hash.hex()}")
+
+    result2 = contract.functions.leerSaludo().call()
+
+    print("-" * 50)
+    print(f"Ultimo saludo: {result2}")
